657d1bea8afc875e8291432ef8155dfe17229f69
Elvis de Aguiar e Silva <>
2019-06-04 22:43:09 -0300

    Quase Conclusão do Bind dos Dados com a View

--- FarmaciaDesktopVbNetElvis/Form1.Designer.vb
+++ FarmaciaDesktopVbNetElvis/Form1.Designer.vb
@@ -23,9 +23,24 @@ Partial Class FrmPrincipal
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.PnlCarregarRelatorios = New System.Windows.Forms.Panel()
-        Me.PnlReports = New System.Windows.Forms.Panel()
         Me.BtnRecarregar = New System.Windows.Forms.Button()
+        Me.PnlReports = New System.Windows.Forms.Panel()
+        Me.TabReports = New System.Windows.Forms.TabControl()
+        Me.PagePeriodo = New System.Windows.Forms.TabPage()
+        Me.GridVendasPorPeriodo = New System.Windows.Forms.DataGridView()
+        Me.PageCategoria = New System.Windows.Forms.TabPage()
+        Me.GridVendasPorCategoria = New System.Windows.Forms.DataGridView()
+        Me.PageProduto = New System.Windows.Forms.TabPage()
+        Me.GridVendasPorProduto = New System.Windows.Forms.DataGridView()
         Me.PnlCarregarRelatorios.SuspendLayout()
+        Me.PnlReports.SuspendLayout()
+        Me.TabReports.SuspendLayout()
+        Me.PagePeriodo.SuspendLayout()
+        CType(Me.GridVendasPorPeriodo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PageCategoria.SuspendLayout()
+        CType(Me.GridVendasPorCategoria, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PageProduto.SuspendLayout()
+        CType(Me.GridVendasPorProduto, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PnlCarregarRelatorios
@@ -33,23 +48,12 @@ Partial Class FrmPrincipal
         Me.PnlCarregarRelatorios.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.PnlCarregarRelatorios.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.PnlCarregarRelatorios.BackColor = System.Drawing.SystemColors.Control
         Me.PnlCarregarRelatorios.Controls.Add(Me.BtnRecarregar)
         Me.PnlCarregarRelatorios.Location = New System.Drawing.Point(0, 502)
         Me.PnlCarregarRelatorios.Name = "PnlCarregarRelatorios"
         Me.PnlCarregarRelatorios.Size = New System.Drawing.Size(785, 57)
         Me.PnlCarregarRelatorios.TabIndex = 0
-        '
-        'PnlReports
-        '
-        Me.PnlReports.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.PnlReports.BackColor = System.Drawing.SystemColors.AppWorkspace
-        Me.PnlReports.Location = New System.Drawing.Point(0, 1)
-        Me.PnlReports.Name = "PnlReports"
-        Me.PnlReports.Size = New System.Drawing.Size(785, 502)
-        Me.PnlReports.TabIndex = 1
         '
         'BtnRecarregar
         '
@@ -59,6 +63,94 @@ Partial Class FrmPrincipal
         Me.BtnRecarregar.TabIndex = 0
         Me.BtnRecarregar.Text = "Recarregar"
         Me.BtnRecarregar.UseVisualStyleBackColor = True
+        '
+        'PnlReports
+        '
+        Me.PnlReports.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PnlReports.BackColor = System.Drawing.SystemColors.Control
+        Me.PnlReports.Controls.Add(Me.TabReports)
+        Me.PnlReports.Location = New System.Drawing.Point(0, 1)
+        Me.PnlReports.Name = "PnlReports"
+        Me.PnlReports.Size = New System.Drawing.Size(785, 502)
+        Me.PnlReports.TabIndex = 1
+        '
+        'TabReports
+        '
+        Me.TabReports.Controls.Add(Me.PagePeriodo)
+        Me.TabReports.Controls.Add(Me.PageCategoria)
+        Me.TabReports.Controls.Add(Me.PageProduto)
+        Me.TabReports.Location = New System.Drawing.Point(43, 28)
+        Me.TabReports.Name = "TabReports"
+        Me.TabReports.SelectedIndex = 0
+        Me.TabReports.Size = New System.Drawing.Size(699, 439)
+        Me.TabReports.TabIndex = 0
+        '
+        'PagePeriodo
+        '
+        Me.PagePeriodo.Controls.Add(Me.GridVendasPorPeriodo)
+        Me.PagePeriodo.Location = New System.Drawing.Point(4, 22)
+        Me.PagePeriodo.Name = "PagePeriodo"
+        Me.PagePeriodo.Padding = New System.Windows.Forms.Padding(3)
+        Me.PagePeriodo.Size = New System.Drawing.Size(691, 413)
+        Me.PagePeriodo.TabIndex = 0
+        Me.PagePeriodo.Text = "Vendas por Período"
+        Me.PagePeriodo.UseVisualStyleBackColor = True
+        '
+        'GridVendasPorPeriodo
+        '
+        Me.GridVendasPorPeriodo.AllowUserToAddRows = False
+        Me.GridVendasPorPeriodo.AllowUserToDeleteRows = False
+        Me.GridVendasPorPeriodo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridVendasPorPeriodo.Location = New System.Drawing.Point(159, 15)
+        Me.GridVendasPorPeriodo.Name = "GridVendasPorPeriodo"
+        Me.GridVendasPorPeriodo.ReadOnly = True
+        Me.GridVendasPorPeriodo.Size = New System.Drawing.Size(349, 376)
+        Me.GridVendasPorPeriodo.TabIndex = 0
+        '
+        'PageCategoria
+        '
+        Me.PageCategoria.Controls.Add(Me.GridVendasPorCategoria)
+        Me.PageCategoria.Location = New System.Drawing.Point(4, 22)
+        Me.PageCategoria.Name = "PageCategoria"
+        Me.PageCategoria.Padding = New System.Windows.Forms.Padding(3)
+        Me.PageCategoria.Size = New System.Drawing.Size(691, 413)
+        Me.PageCategoria.TabIndex = 1
+        Me.PageCategoria.Text = "Vendas por Categoria"
+        Me.PageCategoria.UseVisualStyleBackColor = True
+        '
+        'GridVendasPorCategoria
+        '
+        Me.GridVendasPorCategoria.AllowUserToAddRows = False
+        Me.GridVendasPorCategoria.AllowUserToDeleteRows = False
+        Me.GridVendasPorCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridVendasPorCategoria.Location = New System.Drawing.Point(192, 18)
+        Me.GridVendasPorCategoria.Name = "GridVendasPorCategoria"
+        Me.GridVendasPorCategoria.ReadOnly = True
+        Me.GridVendasPorCategoria.Size = New System.Drawing.Size(306, 376)
+        Me.GridVendasPorCategoria.TabIndex = 1
+        '
+        'PageProduto
+        '
+        Me.PageProduto.Controls.Add(Me.GridVendasPorProduto)
+        Me.PageProduto.Location = New System.Drawing.Point(4, 22)
+        Me.PageProduto.Name = "PageProduto"
+        Me.PageProduto.Size = New System.Drawing.Size(691, 413)
+        Me.PageProduto.TabIndex = 2
+        Me.PageProduto.Text = "Vendas por Produto"
+        Me.PageProduto.UseVisualStyleBackColor = True
+        '
+        'GridVendasPorProduto
+        '
+        Me.GridVendasPorProduto.AllowUserToAddRows = False
+        Me.GridVendasPorProduto.AllowUserToDeleteRows = False
+        Me.GridVendasPorProduto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridVendasPorProduto.Location = New System.Drawing.Point(192, 18)
+        Me.GridVendasPorProduto.Name = "GridVendasPorProduto"
+        Me.GridVendasPorProduto.ReadOnly = True
+        Me.GridVendasPorProduto.Size = New System.Drawing.Size(306, 376)
+        Me.GridVendasPorProduto.TabIndex = 1
         '
         'FrmPrincipal
         '
@@ -71,6 +163,14 @@ Partial Class FrmPrincipal
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Farmácia VB.NET Elvis"
         Me.PnlCarregarRelatorios.ResumeLayout(False)
+        Me.PnlReports.ResumeLayout(False)
+        Me.TabReports.ResumeLayout(False)
+        Me.PagePeriodo.ResumeLayout(False)
+        CType(Me.GridVendasPorPeriodo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PageCategoria.ResumeLayout(False)
+        CType(Me.GridVendasPorCategoria, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PageProduto.ResumeLayout(False)
+        CType(Me.GridVendasPorProduto, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -78,4 +178,11 @@ Partial Class FrmPrincipal
     Friend WithEvents PnlCarregarRelatorios As Panel
     Friend WithEvents PnlReports As Panel
     Friend WithEvents BtnRecarregar As Button
+    Friend WithEvents TabReports As TabControl
+    Friend WithEvents PagePeriodo As TabPage
+    Friend WithEvents GridVendasPorPeriodo As DataGridView
+    Friend WithEvents PageCategoria As TabPage
+    Friend WithEvents GridVendasPorCategoria As DataGridView
+    Friend WithEvents PageProduto As TabPage
+    Friend WithEvents GridVendasPorProduto As DataGridView
 End Class
--- FarmaciaDesktopVbNetElvis/Form1.vb
+++ FarmaciaDesktopVbNetElvis/Form1.vb
@@ -1,4 +1,5 @@
-﻿Imports ApiFarmaciaElvis.Entidades.DTOs
+﻿Imports System.ComponentModel
+Imports ApiFarmaciaElvis.Entidades.DTOs
 Imports ApiFarmaciaElvis.Repositorios
 
 Public Class FrmPrincipal
@@ -6,43 +7,33 @@ Public Class FrmPrincipal
 
     Private ReadOnly ReportsService As ReportsService
 
+    Private VendasPorCategoria As List(Of VendaPorCategoriaDTO)
+
+    Private VendasPorPeriodo As List(Of VendaPorPeriodoDTO)
+
+    Private VendasPorProduto As List(Of VendaPorProdutoDTO)
+
     Private Function ObterConnectionString() As String
         Return System.Configuration.ConfigurationManager.ConnectionStrings(NOME_CONNECTION_STRING).ConnectionString
     End Function
 
+    Private Function ConvertListToBindingSource(Of T)(ByRef lista As IList(Of T)) As BindingSource
+        If lista Is Nothing Then
+            lista = New List(Of T)()
+        End If
+
+        Dim BindingList As BindingList(Of T) = New BindingList(Of T)(lista)
+        Dim BindingSource As BindingSource = New BindingSource(BindingList, Nothing)
+
+        Return BindingSource
+    End Function
+
+
     Private Sub AssociarComponentesVisuaisComDados()
-
+        Me.GridVendasPorCategoria.DataSource = ConvertListToBindingSource(Of VendaPorCategoriaDTO)(Me.VendasPorCategoria)
+        Me.GridVendasPorPeriodo.DataSource = ConvertListToBindingSource(Of VendaPorPeriodoDTO)(Me.VendasPorPeriodo)
+        Me.GridVendasPorProduto.DataSource = ConvertListToBindingSource(Of VendaPorProdutoDTO)(Me.VendasPorProduto)
     End Sub
-
-    Private _VendasPorCategoria As List(Of VendaPorCategoriaDTO)
-    Public Property VendasPorCategoria() As List(Of VendaPorCategoriaDTO)
-        Get
-            Return _VendasPorCategoria
-        End Get
-        Private Set(ByVal value As List(Of VendaPorCategoriaDTO))
-            _VendasPorCategoria = value
-        End Set
-    End Property
-
-    Private _VendasPorPeriodo As List(Of VendaPorPeriodoDTO)
-    Public Property VendasPorPeriodo() As List(Of VendaPorPeriodoDTO)
-        Get
-            Return _VendasPorPeriodo
-        End Get
-        Private Set(ByVal value As List(Of VendaPorPeriodoDTO))
-            _VendasPorPeriodo = value
-        End Set
-    End Property
-
-    Private _VendasPorProduto As List(Of VendaPorProdutoDTO)
-    Public Property VendasPorProduto() As List(Of VendaPorProdutoDTO)
-        Get
-            Return _VendasPorProduto
-        End Get
-        Private Set(ByVal value As List(Of VendaPorProdutoDTO))
-            _VendasPorProduto = value
-        End Set
-    End Property
 
     Public Sub CarregarRelatorios()
         Dim Reports As ReportsDTO = Me.ReportsService.GetReports()
@@ -51,6 +42,8 @@ Public Class FrmPrincipal
             Me.VendasPorCategoria = Reports.VendasPorCategoria
             Me.VendasPorPeriodo = Reports.VendasPorPeriodo
             Me.VendasPorProduto = Reports.VendasPorProduto
+
+            AssociarComponentesVisuaisComDados()
         End If
     End Sub
 
@@ -62,8 +55,7 @@ Public Class FrmPrincipal
         Dim ConnectionString As String = ObterConnectionString()
         Me.ReportsService = New ReportsService(ConnectionString)
 
-        AssociarComponentesVisuaisComDados()
-        'CarregarRelatorios()
+        CarregarRelatorios()
     End Sub
 
     Private Sub BtnRecarregar_Click(sender As Object, e As EventArgs) Handles BtnRecarregar.Click
